c05f15845abb6aa41b8aea2e271cebab7ed1641a
Jusen <dujusen@gmail.com>
2025-09-15 21:52:09 +0800

    Update MoM via varlen

--- mom/__init__.py
+++ mom/__init__.py
@@ -1,24 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from mom.layers import MomGatedDeltaNet, MomLinearAttention, MomGatedSlotAttention, MomGatedLinearAttention
-from mom.models import (MomGatedDeltaNetForCausalLM, MomGatedDeltaNetModel,
-                        MomLinearAttentionForCausalLM, MomLinearAttentionModel,
-                        MomGLAForCausalLM, MomGLAModel,
-                        MomGSAForCausalLM, MomGSAModel)
+from mom.layers import MomAttention
+from mom.models import MomForCausalLM, MomModel
 
 __all__ = [
-    'MomGatedDeltaNet',
-    'MomGatedLinearAttention',
-    'MomGatedSlotAttention',
-    'MomLinearAttention',
-    'MomGatedDeltaNetForCausalLM',
-    'MomGatedDeltaNetModel',
-    'MomGLAForCausalLM',
-    'MomGLAModel',
-    'MomGSAForCausalLM',
-    'MomGSAModel',
-    'MomLinearAttentionForCausalLM',
-    'MomLinearAttentionModel',
+    'Mom',
 ]
 
 __version__ = '0.1'
--- mom/layers/__init__.py
+++ mom/layers/__init__.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .mom_gated_deltanet import MomGatedDeltaNet
-from .mom_gla import MomGatedLinearAttention
-from .mom_gsa import MomGatedSlotAttention
-from .mom_linear_attn import MomLinearAttention
+from .mom import MomAttention
 
 __all__ = [
-    'MomGatedDeltaNet',
-    'MomGatedLinearAttention',
-    'MomGatedSlotAttention',
-    'MomLinearAttention',
+    'MomAttention',
 ]
